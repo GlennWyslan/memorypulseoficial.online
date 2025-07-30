8786267ec4d4794a52b75a798b825e4d1e030e1e
LessyDigital <106035220+GlennWyslan@users.noreply.github.com>
2025-07-30 00:00:31 -0300

    Add files via upload

---
+++ gracias-pp/js/ljrNXe4355465.js
@@ -0,0 +1,20 @@
+(function() {
+          try {
+              const clickeventList = [{"compKey":"7162884","misc":{"type":"button"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey;
+                  const eleType = comp?.misc?.type;
+                  
+                  
+                  
+                  
+                  
+                  
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();
