27607a2d13efa54b25c290fec6a31197507c4313
LessyDigital <106035220+GlennWyslan@users.noreply.github.com>
2025-08-05 15:10:24 -0300

    Add files via upload

---
+++ ds2-k/js/QwsijQ1502499.js
@@ -0,0 +1,20 @@
+(function() {
+          try {
+              const clickeventList = [{"compKey":"bd6608d","misc":{"type":"button"}},{"compKey":"6b87c87","misc":{"type":"text"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey;
+                  const eleType = comp?.misc?.type;
+                  
+                  
+                  
+                  
+                  
+                  
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();
---
+++ oto2-k/js/ynLtvp4015940.js
@@ -0,0 +1,84 @@
+
+      function runDelayedFunctions(data) {
+        try {
+          document.querySelectorAll('.atomicat-delay').forEach(el => el.classList.remove('atomicat-delay'));
+          if(data?.setDisplayed){
+            localStorage.setItem(data?.setDisplayed, true);
+          }
+          
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    (function() {
+          try {
+              const clickeventList = [{"compKey":"2e0c953","misc":{"type":"button"}},{"compKey":"7d0fcc5","misc":{"type":"button"}},{"compKey":"9e6ae51","misc":{"type":"text"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey;
+                  const eleType = comp?.misc?.type;
+                  
+                  
+                  
+                  
+                  
+                  
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();
+          (function() {
+            try {
+              var vturbvideoId = "688d41f0046e088eb878872f";
+              var SECONDS_TO_DISPLAY = 261;
+              var attempts = 0;
+              var elsDisplayed = false;
+              var alreadyDisplayedKey = 'alreadyElsDisplayed261';
+              var alreadyElsDisplayed = localStorage.getItem(alreadyDisplayedKey);
+
+              var showHiddenElements = function () {
+                elsDisplayed = true;
+                runDelayedFunctions();
+                localStorage.setItem(alreadyDisplayedKey, true);
+              };
+              if (alreadyElsDisplayed === 'true') {
+                setTimeout(function () {
+                  showHiddenElements();
+                }, 100);
+              } else {
+                startWatchVideoProgress();
+              }
+              function getVideoInstance() {
+                if (smartplayer.instances.length > 1) {
+                  return smartplayer.instances.find(
+                    (instance) => (instance?.options?.id || instance?.analytics?.player?.options?.id) === vturbvideoId
+                  );
+                }
+                return smartplayer.instances[0];
+              };
+              function startWatchVideoProgress() {
+                if (typeof smartplayer === 'undefined' || !(smartplayer.instances && smartplayer.instances.length)) {
+                  if (attempts >= 10) return;
+                  attempts += 1;
+                  return setTimeout(function () {
+                    startWatchVideoProgress();
+                  }, 1000);
+                }
+                console.log(smartplayer.instances);
+                var videoInstance = getVideoInstance();
+                videoInstance.on('timeupdate', () => {
+                  if (elsDisplayed || videoInstance.smartAutoPlay) return;
+                  console.log("currentTime => " +videoInstance.video.currentTime +" SECONDS_TO_DISPLAY => " +SECONDS_TO_DISPLAY);
+                  if (videoInstance.video.currentTime < SECONDS_TO_DISPLAY) return;
+                  showHiddenElements();
+                });
+              };
+            } catch (error) {
+              console.log(error);
+            }
+            
+          })();
+        
