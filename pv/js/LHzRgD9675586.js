120d96477d8f3ae369eafa92733453a04d981faf
LessyDigital <106035220+GlennWyslan@users.noreply.github.com>
2025-08-25 19:08:45 -0300

    Add files via upload

---
+++ pv/js/LHzRgD9675586.js
@@ -0,0 +1,30 @@
+
+      function atomiShowItems({items}) {
+        items.forEach((item) => {
+          const hiddenItem = [...document.querySelectorAll(`#${item}`), ...document.querySelectorAll(`.${item}`)];
+          console.log("hiddenItem", hiddenItem)
+          if (hiddenItem?.length > 0) {
+            hiddenItem.forEach(item => item.classList.remove("atomicat-delay"));
+          }
+        })
+      }
+    (function() {
+          try {
+              const clickeventList = [{"compKey":"e282ebd","misc":{"type":"button"}},{"compKey":"8447096","misc":{"type":"text"}},{"compKey":"032aa90","misc":{"type":"text"}}];
+    
+    
+              clickeventList.forEach((comp, index) => {
+                  const compKey = comp?.compKey;
+                  const eleType = comp?.misc?.type;
+                  
+                  
+                  
+                  
+                  
+                  
+              });
+    
+          } catch (error) {
+              return error;
+          }
+      })();
